cc6db23e96bebc697999b42dc2cfa505d395e51b
Gobi Mahato <thegobimahato@gmail.com>
2025-08-16 01:17:27 +0530

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,19 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  // Base Next.js rules
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", prettier),
 
-  // Ignore generated Prisma files
+  // Completely ignore generated Prisma files
   {
     files: ["app/generated/prisma/**/*.{ts,js}"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "@typescript-eslint/no-this-alias": "off",
-      "@typescript-eslint/no-require-imports": "off",
-    },
+    rules: {},
   },
 ];
+
+export const ignorePatterns = ["app/generated/prisma/**/*"];
 
 export default eslintConfig;
